6f5d41719df595e1df97d6465a21c9d451b96516
Megan Wilde <miss.megan@gmail.com>
2021-01-19 15:10:52 -0600

    modified models some more, removed auctionuser class, renamed some fields

--- auctions/models.py
+++ auctions/models.py
@@ -14,17 +14,19 @@ class Listing(models.Model):
     imageURL = models.URLField()
     listing_created = models.DateField(auto_now_add=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name="creator")
-    bidders = models.ManyToManyField(User, blank=True, related_name="listings")
     STATUS = Choices('Active', 'Closed')
     status = StatusField()
+    reserve = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    bidders = models.ManyToManyField(User, blank=True, related_name="listings")
 
     def __str__(self):
         return f"{self.title}"
 
+
 class Bid(models.Model):
 
     bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidder")
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bid_on_item")
+    item = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="item", null=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     bid_timestamp = models.DateField(auto_now_add=True)
 
@@ -34,7 +36,7 @@ class Bid(models.Model):
 class Comment(models.Model):
 
     commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="regarding_listing")
+    topic = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="topic")
     comment_timestamp = models.DateField(auto_now_add=True)
 
     def __str__(self):
